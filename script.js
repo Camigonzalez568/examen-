5693f01b94d9c8afd813a99059f0183f31b24b7f
camigonzalez568 <cg3857722@gmail.com>
2024-04-29 10:45:43 -0300

    a

---
+++ script.js
@@ -0,0 +1,20 @@
+const rojo = document.querySelector('.rojo')
+const azul = document.querySelector('.azul')
+const rosa = document.querySelector('.rosa')
+const verde = document.querySelector('.verde')
+
+rojo.addEventListener('click', () =>{
+      alert('tocaste el color rojo ');
+});
+
+azul.addEventListener('click', () =>{
+    alert('tocaste el color azul');
+});
+
+verde.addEventListener('click', () =>{
+    alert('tocaste el color verde');
+});
+
+rosa.addEventListener('click', () =>{
+    alert('tocaste el color rosa');
+});
